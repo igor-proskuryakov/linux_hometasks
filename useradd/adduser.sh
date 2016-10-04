1e201520e3478c0af1e35fab285283e00ae7f851
root <root@localhost.localdomain>
2016-10-04 22:03:11 +0300

    final

--- useradd/adduser.sh
+++ useradd/adduser.sh
@@ -1,12 +1,23 @@
-name=$1
-group=$2
-dir=$3
-password=$4
-flag=0
-echo $group
-grep $group /etc/group | sed s/:.*// | while read line
+#!/bin/bash
+checkgroup()
+{
+	uname=$1
+	group=$2
+	directory=$3
+	passw=$4
+	groupadd -f $group
+	if [[ -n $directory ]]; then dir=" -d $directory"; fi
+	if [[ -n $passw ]]; then password=" -p $passw"; fi
+	command="$uname -g $group $dir $password"
+	useradd $command 
+	if [[ $? -eq 0 ]];then echo "user $uname was added!";fi
+}
+
+
+
+
+cat list.txt | while read line
 do
-let flag=1+1
-echo $flag
+checkgroup $line
 done
-echo $flag
+
