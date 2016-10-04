061b21849e8db6d0170c0e17392f9a7681b28e7c
root <root@localhost.localdomain>
2016-10-05 00:21:41 +0300

    try #2

--- useradd/adduser.sh
+++ useradd/adduser.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+subgroup()
+{
+        local ingroups=$1
+        local subgroups=`echo $ingroups |tr ',' ' '`
+        for i in $subgroups; do groupadd -f $i; done
+}
+
+
 checkgroup()
 {
 	uname=$1
 	group=$2
-	directory=$3
-	passw=$4
+	subgroups=$3
+	directory=$4
+	passw=$5
+	sh=$6
+	subgroup $subgroups
 	groupadd -f $group
-	if [[ -n $directory ]]; then dir=" -d $directory"; fi
-	if [[ -n $passw ]]; then password=" -p $passw"; fi
-	command="$uname -g $group $dir $password"
+	if [[ -n $directory ]]; then dir=" -d $directory";else dir=""; fi
+	if [[ $passw != "none" ]]; then password=" -p $passw"; else password=""; fi
+	if [[ -n $sh ]]; then shell=" -s $sh";else shell=""; fi
+	command="-g $group -G $subgroups $dir $password $shell $uname"
 	useradd $command 
 	if [[ $? -eq 0 ]];then echo "user $uname was added!";fi
 }
+
 
 
 
